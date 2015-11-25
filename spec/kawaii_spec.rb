ffd2af43d0da14399b8a29539ead47f8d9a6c3fd
Martin Bilski <martinb@criticue.com>
2015-11-25 09:21:15 +0100

    Remove failing example spec.

--- spec/kawaii_spec.rb
+++ spec/kawaii_spec.rb
@@ -4,10 +4,6 @@ describe Kawaii do
   it 'has a version number' do
     expect(Kawaii::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
 
 
