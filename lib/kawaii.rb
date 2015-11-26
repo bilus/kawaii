2c11dd74f1750ca6b530bdc4a79e2feaf8974601
Martin Bilski <martinb@criticue.com>
2015-11-26 12:48:10 +0100

    Extract route building code into Router. Add descriptions to all classes.

--- lib/kawaii.rb
+++ lib/kawaii.rb
@@ -2,24 +2,17 @@
 require 'rack'
 
 module Kawaii
-  class RouteBuilder
-    def get(route, &block)
-    end
-
-    def build
-      []
-    end
-  end
-
+  # A single route. Provides matching while behaving like a regular Rack app (Route#call).
+  #
   class Route
-    def initialize(*paths, &block)
-      @path = File.join(*paths)
+    def initialize(path, &block)
+      @path = path
       @block = block
     end
     
-    def matches?(env)
-      puts "matches? #{@path} #{env[Rack::PATH_INFO]}" 
-      @path == env[Rack::PATH_INFO]
+    def match(env)
+      puts "Route#match #{@path} #{env[Rack::PATH_INFO]}" 
+      self if @path == env[Rack::PATH_INFO]
     end
 
     def call(env)
@@ -27,6 +20,9 @@ module Kawaii
     end
   end
 
+  # Mixins for starting a self-contained server.
+  # At the moment hard-coded to use WEBrick.
+  #
   module ServerMethods
     def start! # TODO: Support other handlers http://www.rubydoc.info/github/rack/rack/Rack/Handler
       Rack::Handler.get("WEBrick").run(self, :Port => 8092) do |s| # TODO: Hard-coded port number.
@@ -50,21 +46,100 @@ module Kawaii
     end
   end
 
-  class RouteContext
-    def initialize(*paths, app)
-      @paths = paths
-      @app = app
+  # Core route-building and matching.
+  #
+  # These functions can be used both in a class inheriting from Kawaii::Base and with `main` object.
+  #
+  # Example
+  #
+  # class MyApp < Kawaii::Base
+  #   get '/' do
+  #     'Hello, world'
+  #   end
+  # end
+  #
+  # or
+  #
+  # get '/' do
+  #   'Hello, world'
+  # end
+  #
+  class Router
+    ANY_METHOD = :any
+    
+    def initialize
+      @routes = Hash.new {|h, k| h[k] = []}
     end
 
     def get(path, &block)
-      @app.add_route!(Rack::GET, *@paths, path, &block) 
+      add_route!(Rack::GET, Route.new(path, &block)) 
     end
 
     def context(path, &block)
-      RouteContext.new(*@paths, path, @app).instance_eval(&block)
+      ctx = RouteContext.new(path)
+      add_route!(Router::ANY_METHOD, ctx)
+      ctx.instance_eval(&block)
+    end
+    
+    def match(env)
+      all_routes = @routes[env[Rack::REQUEST_METHOD]] + @routes[ANY_METHOD] # TODO: Performance.
+      all_routes.lazy.map {|r| r.match(env)}.find {|r| !r.nil?} # Lazy to avoid unnecessary calls to #match.
+    end
+
+    protected
+
+    def add_route!(method, router)
+      puts "add_route! #{self.inspect}"
+      @routes[method] << router 
     end
   end
-  
+
+  # Implementation of nested routes generated via Router#context.
+  #
+  # Example
+  #
+  # context '/foo' do
+  #   get '/bar' do
+  #   end
+  # end
+  #
+  # Is a rough equivalent of:
+  #
+  # ctx = RouteContext.new('/foo')
+  # ctx.get '/bar' do
+  # end
+  #
+  class RouteContext < Router
+    def initialize(path)
+      super()
+      @path = path
+    end
+
+    def match(env)
+      puts "RouteContext#match #{self.inspect} #{env.inspect}"
+      remaining_path = match_path(env[Rack::PATH_INFO])
+      if remaining_path
+        super(env.merge(Rack::PATH_INFO => remaining_path))
+      end
+    end
+
+    def match_path(path_info)
+      compiled_path = Regexp.new("^#{@path}(.*)")
+      path_info.match(compiled_path).to_a.last
+    end
+  end
+
+  # Base class for all Kawaii applications. Inherit from this class to create a modular
+  # application.
+  #
+  # Example
+  #
+  # class MyApp < Kawaii::Base
+  #   get '/' do
+  #     'Hello, world'
+  #   end
+  # end
+  #
   class Base
     def initialize(downstream_app = nil) # TODO: Downstream app.
     end
@@ -77,13 +152,19 @@ module Kawaii
     class << self
       include ServerMethods
 
+      # TODO: Generate dynamically.
       def context(path, &block)
-        RouteContext.new(path, self).instance_eval(&block)
+        router.context(path, &block)
       end
       
       def get(path, &block)
-        add_route!(Rack::GET, path, &block) 
+        router.get(path, &block)
       end
+
+      def router
+        @router ||= Router.new
+      end
+
 
       # Make it runnable via `run MyApp`.
       def call(env)
@@ -92,19 +173,8 @@ module Kawaii
       end
       
       def match(env)
-        ensure_routes!
-        puts "match #{self.inspect} #{env.inspect}"
-        @routes[env[Rack::REQUEST_METHOD]].find {|r| r.matches?(env)}
-      end
-
-      def add_route!(method, *paths, &block)
-        puts "add_route! #{self.inspect}"
-        ensure_routes!
-        @routes[method] << Route.new(*paths, &block)
-      end
-
-      def ensure_routes!
-        @routes ||= Hash.new {|h, k| h[k] = []}
+        puts "Base#match #{self.inspect} #{env.inspect}"
+        router.match(env)
       end
     end
     
@@ -115,6 +185,26 @@ module Kawaii
     end
   end
 
+  # Class used to implement a standalone Kawaii app generated with top-level route helpers
+  # (e.g. monkey-patched onto the `main` object).
+  #
+  # This lets you create a .rb file containing just route definitions and run it with ruby command.
+  #
+  # Example (test.rb)
+  #
+  # require 'kawaii'
+  #
+  # get '/' do
+  #   'Hello, world'
+  # end
+  #
+  # $ ruby -r kawaii test.rb 
+  # [2015-11-25 23:36:28] INFO  WEBrick 1.3.1
+  # [2015-11-25 23:36:28] INFO  ruby 2.1.2 (2014-05-08) [x86_64-darwin13.0]
+  # [2015-11-25 23:36:28] INFO  WEBrick::HTTPServer#start: pid=43750 port=8092
+  #
+  # See examples/hello_world.rb and 'Running examples' in the Readme.
+  # 
   class SingletonApp < Base
     class << self
       def maybe_start!
@@ -139,14 +229,14 @@ module Kawaii
   end
 end
 
-# Helpers to use directly in a .rb file without using a class inheriting from Kawaii::Base
+# Helpers you use directly in a .rb file without using a class inheriting from Kawaii::Base
 #
 # Example
 #
 # get '/' do
 #    'Hello, world'
 # end
-
+#
 class << self
   # TODO: Use define_method
   def get(*args, &block)
@@ -159,7 +249,8 @@ class << self
 end
 
 
-# For self-contained execution without config.ru.
+# For self-contained execution without config.ru. See the description of `SingletonApp` above.
+#
 Kawaii::SingletonApp.maybe_start!
 
 
